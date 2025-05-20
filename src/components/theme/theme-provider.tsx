ed5d746cae4a08fb0cc1235024fcc15d657d7bfe
Rkgandhi00 <rkgandhi00@gmail.com>
2025-05-19 23:41:39 -0400

    fixed theme issue on prod

--- src/components/theme/theme-provider.tsx
+++ src/components/theme/theme-provider.tsx
@@ -5,5 +5,14 @@ import * as React from "react"
 import { ThemeProvider as NextThemesProvider, ThemeProviderProps } from "next-themes"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="light"
+      enableSystem={true}
+      {...props}
+    >
+      {children}
+    </NextThemesProvider>
+  )
 }
